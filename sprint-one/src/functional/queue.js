c95b4d026b6c2642b88dd3d3a2be4f79ba6adba2
matgbx <matbagnall@gmail.com>
2018-01-17 16:28:58 -0800

    committing completed functional stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
+  var first = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size++;
+    
   };
 
-  someInstance.dequeue = function() {
+  someInstance.dequeue = function() {  
+    if (someInstance.size() > 0) {
+      var zero = storage[first];
+      delete storage[first];
+      first++;
+      return zero;
+    } 
   };
 
   someInstance.size = function() {
+    return size - first;
   };
 
   return someInstance;
