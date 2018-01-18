400d02f867833a614b2659ff95d80ee6c84dec74
Mat Bagnall <matbagnall@gmail.com>
2018-01-17 22:55:53 -0800

    committing completed Protoypal Class Stack and Queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.storage = {};
+  instance.first = 0;
+  instance.count = 0;
+
+  return instance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+	this.storage[this.count] = value;
+	this.count++;
+};
+
+queueMethods.dequeue = function() {
+	if ((this.count - this.first) > 0) {
+		var item = this.storage[this.first];
+		delete this.storage[this.first];
+		this.first++;
+		return item;
+	}
+};
+
+queueMethods.size = function() {
+	return this.count - this.first;
+};
 
