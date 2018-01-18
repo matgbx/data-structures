09c7204922b7cb592c7d091cf6dac5ada6cd2e5a
matgbx <matbagnall@gmail.com>
2018-01-18 15:49:07 -0800

    linkedList push

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,44 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+    if (!list.head) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node; 
+    }
   };
 
   list.removeHead = function() {
+    var headValue = list.head.value;
+    var nextHead = list.head.next;
+    delete list.head;
+    list.head = nextHead;    
+    return headValue; 
   };
 
   list.contains = function(target) {
+    var item = list.head;
+    
+    if (item.value === target) {
+      return true;
+    }
+    var test = false;
+    var nextItem = item.next;
+    while (test === false) {
+      if (!nextItem) {
+        return false;
+      } else if (nextItem.value !== target) {
+        test = false;
+        nextItem = nextItem.next;
+      } else if (nextItem.value === target) {
+        test = true;
+      }
+    }
+    
+    return test;
   };
 
   return list;
