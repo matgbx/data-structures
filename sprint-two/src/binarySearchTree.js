60b7c3c4d5bcf24050cc2e1c6dc265596583454d
matgbx <matbagnall@gmail.com>
2018-01-19 18:47:08 -0800

    committing completed Hash Table

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,28 @@
 var BinarySearchTree = function(value) {
+  var tree = {};
+  tree.value = value;
+  tree.storage = {};
+  tree.children = [];
+  _.extend(tree, treeMethods);
+   
+  return tree;   
 };
+
+var treeMethods = {};
+
+treeMethods.insert = function() {
+
+};
+treeMethods.contains = function() {
+
+};
+treeMethods.depthFirstLog = function() {
+  
+};
+            
+
+
+
 
 
 /*
