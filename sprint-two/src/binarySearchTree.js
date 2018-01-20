dade87856741912549df2589a0e03f023e9f3d43
matgbx <matbagnall@gmail.com>
2018-01-20 10:34:24 -0800

    committing all completed basic requirements for sprint 2

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -12,41 +12,51 @@ var BinarySearchTree = function(value) {
 var newTreeMethods = {};
 
 newTreeMethods.insert = function(num) {
-	var findPoint = function(obj, num) {
-		if (num < obj.value) {
-			if (!obj.left) {
-				obj.left = BinarySearchTree(num);
-			} else {
-				findPoint(obj.left, num);
-			}
-		} else if (num > obj.value) {
-			if (!obj.right) {
-				obj.right = BinarySearchTree(num);
-			} else {
-				findPoint(obj.right, num);
-			}		
-		}
-	};
-	findPoint(this, num);
+  var findPoint = function(obj, num) {
+    if (num < obj.value) {
+      if (!obj.left) {
+        obj.left = BinarySearchTree(num);
+      } else {
+        findPoint(obj.left, num);
+      }
+    } else if (num > obj.value) {
+      if (!obj.right) {
+        obj.right = BinarySearchTree(num);
+      } else {
+        findPoint(obj.right, num);
+      }		
+    }
+  };
+  findPoint(this, num);
 };
 
 newTreeMethods.contains = function(target) {
-	var result = false;
-	var search = function(obj, target) {
-		if (this.value === target) {
-			result = true;
-		} else if (target < this.value && this.left) {
-			search(this.left);
-		} else if (target > this.value && this.right) {
-			search(this.right);
-		} 
-	}
-	search(this, target);
-	return result;
+  var result = false;
+  var find = function(obj, target) {
+    if (obj.value === target) {
+      result = true;
+    }
+    if (obj.left) {
+      find(obj.left, target);
+    }
+    if (obj.right) {
+      find(obj.right, target);
+    }
+  };
+  find(this, target);
+  return result;
 };
-
 newTreeMethods.depthFirstLog = function(cb) {
-  
+  var each = function(obj, cb) {
+    cb(obj.value);
+    if (obj.left) {
+      each(obj.left, cb);
+    }
+    if (obj.right) {
+      each(obj.right, cb);
+    }
+  };
+  each(this, cb);
 };
             
 
